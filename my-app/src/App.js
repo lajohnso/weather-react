42df9d1898ef5fe83fe30cf4c43a40cb16b2283e
Lars-August Udnesseter Johnson <Lars-August@Lars-Augusts-MacBook-Pro.local>
2019-03-26 01:14:32 +0100

    added graphics. Status: Potentially shipable

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
 
-import Titles from "./components/titles/Titles";
-import Weather from "./components/weather/Weather";
 import Form from "./components/form/Form";
+import Weather from "./components/weather/Weather";
+import Titles from "./components/titles/Titles";
 
-const API_KEY='a4451a98f72e5044e5d46ec974d2b9f4';
 
-class App extends Component {
+const API_KEY = "a4451a98f72e5044e5d46ec974d2b9f4";
 
+class App extends React.Component {
   state = {
     temperature: undefined,
     city: undefined,
@@ -16,60 +15,61 @@ class App extends Component {
     humidity: undefined,
     description: undefined,
     error: undefined
-
   }
-
-
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-
-    if (city && country){
-    console.log(data);
-
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error:""
-
-    })
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the values!"
+      });
+    }
   }
-  else{
-    this.setState({
-      temperature: undefined,
-      city: undefined,
-      country: undefined,
-      humidity: undefined,
-      description: undefined,
-      error:"Please enter the values!"
-  })
-}
-}
   render() {
     return (
       <div>
-        <Titles />
-        <p>Find out weather, temperatures and more...</p>
-        <Form getWeather={this.getWeather}/>
-        <Weather
-        temperature={this.state.temperature}
-        city={this.state.city}
-        country={this.state.country}
-        humidity={this.state.humidity}
-        description={this.state.description}
-        error={this.state.error}
-        />
-
-
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-sm-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    humidity={this.state.humidity}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
-}
+};
 
 export default App;
--- my-app/src/components/titles/Titles.js
+++ my-app/src/components/titles/Titles.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Titles extends Component {
-  render() {
-    return (
+const Titles = () =>(
+
+
       <div>
-        <h1>Titles component</h1>
-      </div>
-    );
-  }
-}
+		    <h1 className="title-container__title">Check the weather!</h1>
+		    <h3 className="title-container__subtitle">Search for weather conditions around the world!</h3>
+	    </div>
+
+)
 
 export default Titles;
